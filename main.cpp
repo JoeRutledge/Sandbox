8635c41c5a762c2b2016851ff3663d5c6ad89f1b
Duncan Webb <duncwebb@googlemail.com>
2013-08-02 11:32:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 int main(void)
 {
     std::cout << "Let's get this party started." << std::endl;
+    
+    std::cout << "I have added this and need it to be reviewed" << std::endl;
+    
     return 0;
 }
 
